0669ae703b537187eb02fa8282bbde99a288df3f
sreemo50 <sree2sree02@gmail.com>
2018-04-13 10:17:20 +0530

    gradle eclipse commit

---
+++ src/test/Tests/Common/AbstractTest.java
@@ -0,0 +1,75 @@
+package Common;
+
+import PLAZA.PlazaC;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+public abstract class AbstractTest {
+	protected WebDriver seleniumWebDriver;
+	protected PLAZASITE plaza;
+	
+  @Parameters("browser")
+  @BeforeClass
+  public void ConnectToDeviceOrBrowser(String browser) throws MalformedURLException {
+  //public void ConnectToDeviceOrBrowser() throws MalformedURLException {
+	  
+	  if(browser.equals("IE")){
+		  System.setProperty("webdriver.InternetExplorer.driver","F:\\supportsoftware\\geckodriver.exe");
+		  this.seleniumWebDriver=SeleniumWebDriverBuilder.foIEDriverWebDriverWebDriverrDriver().withLink(new URL(PlazaC.siteURL)).withPlatform("").withVersion("").build();		  
+		  
+	  }
+	  else if(browser.equals("Chrome")){
+	
+		  ChromeOptions options = new ChromeOptions();
+		  options.addArguments("--disable-notifications");
+		  //System.setProperty("webdriver.chrome.driver","F:\\supportsoftware\\chromedriver.exe");
+		  System.setProperty("webdriver.chrome.driver","F:\\supportsoftware\\chromedriver.exe");
+		  this.seleniumWebDriver=SeleniumWebDriverBuilder.foChromeDriverWebDriverWebDriverrDriver().withLink(new URL(PlazaC.siteURL)).withPlatform("").withVersion("").withOption(options).build();
+		  
+	  }
+	  else if(browser.equals("FireFox")){
+		  
+		  System.setProperty("webdriver.gecko.driver","F:\\supportsoftware\\geckodriver.exe");
+		  this.seleniumWebDriver=SeleniumWebDriverBuilder.foFirefoxDriverWebDriverrDriver().withLink(new URL(PlazaC.siteURL)).withPlatform("").withVersion("").build();
+		  
+	  }
+	  else
+	  {
+		  System.out.println("The browser doesnot support");
+	  }
+	  seleniumWebDriver.get(PlazaC.siteURL);
+	  seleniumWebDriver.manage().window().maximize();
+	  plaza=new PLAZASITE(seleniumWebDriver);
+  }
+
+ /* @BeforeSuite
+  public void beforeSuite() throws IOException {
+	  File fileExcel=new File("");
+	  FileInputStream FIP=new FileInputStream(fileExcel);
+	  
+  }
+*/  
+ /*@AfterSuite
+  public void beforeSuite() {
+	  seleniumWebDriver.quit();
+  }
+*/ 
+ 
+  
+  //add data source to each of the test files
+  
+  
+
+}
---
+++ src/test/Tests/Tests/HomeTest.java
@@ -0,0 +1,44 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+
+
+import Common.AbstractTest;
+import Page.HomePage;
+
+
+public class HomeTest extends AbstractTest {
+
+
+	public HomeTest()
+	{
+		
+	}
+	
+	@Test(priority=0)
+	public void Login() {
+		plaza.HomeScreen().emailentry("sree2sree02@gmail.com");		
+		plaza.HomeScreen().passcodeentry("123@sreekanth");
+		plaza.HomeScreen().clickOnLogin();
+	  }
+	
+	@Test(priority=1,dependsOnMethods="Login")
+	public void ValueCheking() {
+		plaza.EntryScreen().namecheck("Sreekanth");
+	  }
+	
+	@Test(priority=2)
+	public void Marketplace() {
+		plaza.EntryScreen().NameClick();		
+	  }
+	
+	
+	
+	
+	
+	}
+
